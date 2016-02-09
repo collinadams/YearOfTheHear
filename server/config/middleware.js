d5f4b345cb200f6fd020930fa16790fe4722d42f
MKS-Student <mks.student@hackreactor.com>
2016-02-09 11:36:37 -0800

    created middleware to for route determination

---
+++ server/config/middleware.js
@@ -0,0 +1,14 @@
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+
+module.exports = function(app, express){
+  var factRouter = express.Router();
+
+  app.use(bodyParser.urlencoded({extended: true}));
+  app.use(bodyParser.json());
+  app.use(express.static(__dirname + '/../../client'));
+
+  app.use('/api/facts', factRouter);
+
+  require('..facts/factRoutes.js')(factRouter);
+};
