caf0556ac117ede13fd7bb1035afe6241f7bc182
MKS-Student <mks.student@hackreactor.com>
2016-02-08 16:17:57 -0800

    make fetch route to numbers api

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var request = require('request');
 
 var app = express();
 
@@ -14,6 +15,17 @@ app.use(bodyParser.urlencoded({ extend: true }));
 app.get('/', function(req, res){
   res.send('hello world');
 });
+
+app.get('/number', function(req, res){
+  request('https://numbersapi.p.mashape.com/{year}/year', function(error, response, data){
+    if(error){
+      console.log('error fetching numbersapi');
+      return;
+    }else if(!error && response.statusCode === 200){
+      res.send('data');
+    }
+  })
+})
 
 console.log('Server now listening on port: ' + port);
 
