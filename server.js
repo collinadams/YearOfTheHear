abeed5b95ba839df1c448004b99badf5abf7852c
MKS-Student <mks.student@hackreactor.com>
2016-02-08 16:51:17 -0800

    commented out api request to test server

--- server.js
+++ server.js
@@ -16,21 +16,20 @@ app.get('/', function(req, res){
   res.send('hello world');
 });
 
-var options = {
-  url: 'https://numbersapi.p.mashape.com/1492/year',
-  headers: {
-    'X-Mashape-Key': 'fO6LEPSj1Ymsha2VU0tZiKZoubJmp1SKQP1jsnSrLLFLQBFA6n',
-    'Accept': 'text/plain'
-  }
-};
+// var options = {
+//   url: 'https://numbersapi.p.mashape.com/1492/year',
+//   headers: {
+//     'X-Mashape-Key': 'fO6LEPSj1Ymsha2VU0tZiKZoubJmp1SKQP1jsnSrLLFLQBFA6n',
+//     'Accept': 'text/plain'
+//   }
+// };
 
-app.get('/number', function(req, res){
-  request(options, function(error, response, body){
-    console.log('body');
-    res.send('body');
-    }
-  })
-});
+// app.get('/number', function(req, res){
+//   request(options, function(error, response, body){
+//     console.log('body');
+//     res.send('body');
+//   })
+// });
 
 console.log('Server now listening on port: ' + port);
 
