4e1a127b251486fdf949c916667292b58024c3d0
MKS-Student <mks.student@hackreactor.com>
2016-02-08 16:44:14 -0800

    customized get header to api

--- server.js
+++ server.js
@@ -16,16 +16,21 @@ app.get('/', function(req, res){
   res.send('hello world');
 });
 
+var options = {
+  url: 'https://numbersapi.p.mashape.com/1492/year',
+  headers: {
+    'X-Mashape-Key': 'fO6LEPSj1Ymsha2VU0tZiKZoubJmp1SKQP1jsnSrLLFLQBFA6n',
+    'Accept': 'text/plain'
+  }
+};
+
 app.get('/number', function(req, res){
-  request('https://numbersapi.p.mashape.com/1492/year', function(error, response, data){
-    if(error){
-      console.log('error fetching numbersapi');
-      return;
-    }else if(!error && response.statusCode === 200){
-      res.send('data');
+  request(options, function(error, response, body){
+    console.log('body');
+    res.send('body');
     }
   })
-})
+});
 
 console.log('Server now listening on port: ' + port);
 
